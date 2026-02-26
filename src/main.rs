698607d62a252da9eea300ac04cafc97bbd99ac9
lefty <geza.balogh@trayport.com>
2026-02-26 20:48:22 +0100

    initial solution bud

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,47 @@ use std::io::{self, BufRead};
  */
 
 fn isNonTrivialRotation(s1: &str, s2: &str) -> bool {
-	let vecDeq1 = std::collections::VecDeque::from(vec![s1.chars()]);
-	let vecDeq2 = std::collections::VecDeque::from(vec![s2.chars()]);
-	println!("{:?} vs a{:?}", vecDeq1, vecDeq1);
-	//println!("{:?}", vecDeq1.iter().eq(&'a'));
-	true
+
+	if s1.eq_ignore_ascii_case(s2) {
+		return true;
+	}
+
+	let mut vecDeq1 = std::collections::VecDeque::new();
+	for x in s1.chars() {
+		vecDeq1.push_back(x);
+	};
+
+	let mut vecDeq2 = std::collections::VecDeque::new();
+	for x in s2.chars() {
+		vecDeq2.push_back(x);
+	};
+
+
+
+	println!("{:?} ", vecDeq1);
+	println!("{:?} ", vecDeq2);
+	for x in 0..vecDeq1.len()-1 {
+		let mut vecDeq3 = std::collections::VecDeque::new();
+		let mut c = vecDeq2.pop_front();
+		println!("what is popped: {:?} ", &c);
+		if c != Option::from(vecDeq1[x]) {
+			vecDeq3.push_back(c.take().unwrap());
+			vecDeq2.append(&mut vecDeq3);
+		}
+		else {
+			vecDeq3.push_back(c.take().unwrap());
+		}
+
+	}
+
+	vecDeq1.iter().collect::<String>() == vecDeq2.iter().collect::<String>()
 
 }
 
 
 
 fn main() {
+	println!("\x1b[2J\x1b[H\x1b[3J");
 	let stdin = io::stdin();
 	let mut stdin_iterator = stdin.lock().lines();
 
--- src/tests.rs
+++ src/tests.rs
@@ -1,1 +1,9 @@
+#[cfg(test)]
+#[test]
+fn so () {
+	use super::*;
 
+	assert_eq!(isNonTrivialRotation("abcd", "abcd"), true);
+	assert_eq!(isNonTrivialRotation("abcd", "bcda"), true);
+	assert_eq!(isNonTrivialRotation("abcd", "abce"), false);
+}
