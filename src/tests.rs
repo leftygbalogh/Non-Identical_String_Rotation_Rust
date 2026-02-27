76d35de9c39579ec9fec97993f60d3862264d417
lefty <geza.balogh@trayport.com>
2026-02-27 09:38:09 +0100

    TC 8, 10, 13, 15 fail, the rest pass

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,19 @@
 #[cfg(test)]
 #[test]
+
 fn so () {
 	use super::*;
 
 	assert_eq!(isNonTrivialRotation("abcd", "abcd"), true);
 	assert_eq!(isNonTrivialRotation("abcd", "bcda"), true);
 	assert_eq!(isNonTrivialRotation("abcd", "abce"), false);
+}
+#[test]
+fn rt () {
+	use super::*;
+	let data = [("asd", "asd", true)];
+
+	for d in data.iter() {
+		assert_eq!(isNonTrivialRotation(d.0, d.1), d.2);
+	}
 }
