af1d50ab9226d31e3fa6a7627c8204706fabc060
lefty <geza.balogh@trayport.com>
2026-03-01 19:15:48 +0100

    experimental changes... did not work too well

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod tests;
-
-
 use std::io::{self, BufRead};
-
 
 
 /*
@@ -18,8 +15,7 @@ fn isNonTrivialRotation(s1: &str, s2: &str) -> bool {
 	use std::collections::VecDeque;
 
 	if s1.eq_ignore_ascii_case(s2) {
-		return false;
-	}
+		return false;} //return early is strings are identical
 
 	let mut vecDeq1 = std::collections::VecDeque::new();
 	for x in s1.chars() {
@@ -31,24 +27,15 @@ fn isNonTrivialRotation(s1: &str, s2: &str) -> bool {
 		vecDeq2.push_back(x);
 	};
 
-
-
-	// println!("{:?} ", vecDeq1);
-	// println!("{:?} ", vecDeq2);
 	let mut index = 0;
 	let mut vecDeq3:VecDeque<char> = std::collections::VecDeque::new();
 
 	while index < vecDeq1.len() {
 
 		let mut c = vecDeq2.pop_front();
-		//println!("index is: {} what is popped: {:?}", index, &c);
 		if &c != &Option::from(vecDeq1[index]) {
-			// println!("character {:?} ", &c);
-			// println!("VD2 before: {:?} ", &vecDeq2);
 			vecDeq3.push_back(c.unwrap().to_ascii_lowercase());
-//			println!("VD3 intermediate: {:?} ", &vecDeq3);
 			vecDeq2.append(&mut vecDeq3);
-//			println!("VD2 intermediate: {:?} ", &vecDeq2);
 			if &vecDeq1.iter().collect::<String>() == &vecDeq2.iter().collect::<String>()
 			{
 				return true;
@@ -61,10 +48,7 @@ fn isNonTrivialRotation(s1: &str, s2: &str) -> bool {
 		index += 1;
 
 	}
-//	println!("D1: {:?}", vecDeq1);
-//	println!("D2: {:?}", vecDeq2);
 	vecDeq1.iter().collect::<String>() == vecDeq2.iter().collect::<String>()
-
 }
 
 
